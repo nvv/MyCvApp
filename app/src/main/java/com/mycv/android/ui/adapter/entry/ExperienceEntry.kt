24d7311a43c5dae64cfaada5b45f2ee6c9e76658
vladnamashko <vnamashko@gmail.com>
2019-06-07 01:39:36 -0700

    forgotten margin.

--- app/src/main/java/com/mycv/android/ui/adapter/entry/ExperienceEntry.kt
+++ app/src/main/java/com/mycv/android/ui/adapter/entry/ExperienceEntry.kt
@@ -2,6 +2,4 @@ package com.mycv.android.ui.adapter.entry
 
 import com.mycv.android.data.model.WorkExperience
 
-class ExperienceEntry(titleValue: String, val experienceEntries : List<WorkExperience>) : TitledEntry(titleValue) {
-
-}
+class ExperienceEntry(titleValue: String, val experienceEntries : List<WorkExperience>) : TitledEntry(titleValue)
